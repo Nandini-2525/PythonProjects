ba41627b1b78a0821ea45638c1aa30f199eddde7
Nandini-2525 <101392626+Nandini-2525@users.noreply.github.com>
2023-09-01 22:11:47 +0530

    Create calculation.py
    The operations like (+, -, *, /, //, %) to done the arithmetic  operation

---
+++ calculation.py
@@ -0,0 +1,23 @@
+print("Provide the numbers to perform operations")
+input()
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+c = a+b
+print("\nSum is", c)
+
+c = a-b
+print("subtraction is", c)
+
+c = a*b
+print("multiplication is ", c)
+
+c = a/b
+print("division is", c)
+
+c = a//b
+print("division 2 is", c)
+
+c = a % b
+print("modulus is", c)
