f6c73d159714414a981d74562daed11047c2bcac
Nandini-2525 <101392626+Nandini-2525@users.noreply.github.com>
2023-09-14 12:30:06 +0530

    Created Basic salary program

---
+++ basicSalary.py
@@ -0,0 +1,13 @@
+
+# bs = int(input("Enter salary"))
+# if bs >= 10000:
+#     hra= bs *10/100
+#     ta = bs*5/100
+#     bonus = 10000
+# else:
+#     hra =1500
+#     ta=5000
+#     bonus= 9000
+    
+# netsal=bs + hra + ta + bonus
+# print(f"{bs} \t {hra}\t{ta}\t{bonus}\t{netsal}")
