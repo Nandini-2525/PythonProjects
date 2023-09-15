cda5c33cf61500e696472d89fceda9ea0470e576
Nandini-2525 <101392626+Nandini-2525@users.noreply.github.com>
2023-09-15 11:04:29 +0530

    Create calculator simple file

---
+++ oper.py
@@ -0,0 +1,20 @@
+# accept 2 no and 1 operator from user
+# and corresponding operator performs operation
+
+a= int(input("Enter number"))
+opr = input("Enter any operator")
+b= int(input("Enter number"))
+
+
+if opr=='+':
+    print(a+b)
+elif opr=='-':
+    print(a-b)
+elif opr=='*':
+    print(a*b)
+elif opr=='/':
+    print(a/b)
+elif opr=='%':
+    print(a%b)
+else:
+    print("enter a valid operator")
